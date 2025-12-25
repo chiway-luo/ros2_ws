a5d2aace61152264a3670699ad8d2f34e6c634d8
sm_PI_4b_chiway <chiwayfpv@gmail.com>
2025-12-25 11:55:51 +0800

    成功启动

--- src/mycar/laser/sllidar_ros2/scripts/create_udev_rules.sh
+++ src/mycar/laser/sllidar_ros2/scripts/create_udev_rules.sh
@@ -3,11 +3,13 @@
 echo "remap the device serial port(ttyUSBX) to  rplidar"
 echo "rplidar usb connection as /dev/rplidar , check it using the command : ls -l /dev|grep ttyUSB"
 echo "start copy rplidar.rules to  /etc/udev/rules.d/"
-colcon_cd rplidar_ros2
-sudo cp scripts/rplidar.rules  /etc/udev/rules.d
+# colcon_cd rplidar_ros2
+sudo cp rplidar.rules  /etc/udev/rules.d
 echo " "
 echo "Restarting udev"
 echo ""
-sudo service udev reload
-sudo service udev restart
+# sudo service udev reload
+# sudo service udev restart
+sudo udevadm control --reload-rules
+sudo udevadm trigger
 echo "finish "
