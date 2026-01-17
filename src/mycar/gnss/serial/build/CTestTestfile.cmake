ca59d8e7f519d4c9386c014fb3b8f543a0b565ba
sm_PI_4b_chiway <chiwayfpv@gmail.com>
2026-01-17 15:13:03 +0800

    gnss相关文件

---
+++ src/mycar/gnss/serial/build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/chiway/ros2_capa/src/mycar/gnss/serial
+# Build directory: /home/chiway/ros2_capa/src/mycar/gnss/serial/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
