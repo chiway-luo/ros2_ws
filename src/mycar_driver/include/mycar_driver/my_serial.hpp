dea644b31f5c574d026fa2e416aabe0ecffbc0c6
chiway_Luo <chiwayfpv@gmail.com>
2026-03-01 00:10:38 +0800

    完成hpp中构造函数的参数添加,以及driver.cpp中串口通信对象实例化时传入参数

--- src/mycar_driver/include/mycar_driver/my_serial.hpp
+++ src/mycar_driver/include/mycar_driver/my_serial.hpp
@@ -3,6 +3,9 @@
 
 #include <iostream>
 
+#include <boost/asio.hpp>
+#include <string>
+
 /* 
     串口通信功能点分析:
         1.打开串口 -- 构造函数实现
@@ -21,7 +24,7 @@ namespace my_serial{
 class SerialPortComm{
 public:
     //打开串口
-    SerialPortComm();
+    SerialPortComm(const std::string& port,int baud_rate,int character_size);//串口号,波特率,数据位
     //释放资源
     ~SerialPortComm();
 
@@ -37,13 +40,58 @@ public:
     //写出舵机控制指令
     void write_servo_control();
 
+    //异常处理函数
+    int handle_error(const std::string &msg,const boost::system::error_code &error);
+
+private:
+    boost::asio::io_context io;//io上下文对象
+    boost::asio::serial_port serial;//串口通信对象
+    boost::system::error_code error;//异常处理对象
+    
 
 };
 
-SerialPortComm::SerialPortComm(){
+//串口号 /dev/mycar
+SerialPortComm::SerialPortComm(const std::string& port,int baud_rate,int character_size) : io(), serial(io){//初始化串口对象
     std::cout << "打开串口" << std::endl;
+    //打开串口
+    serial.open(port,error);
+    //抛出运行时异常
+    if(error)   throw std::runtime_error("无法打开串口: " + port + " " + error.message());
+        
+        
+
+    //设置通信参数
+    serial.set_option(boost::asio::serial_port_base::baud_rate(baud_rate),error);//波特率
+    if(error) throw std::runtime_error("设置波特率失败: " + error.message());
+    serial.set_option(boost::asio::serial_port_base::character_size(character_size),error);//数据位 一个字节 8bits
+    if(error) throw std::runtime_error("设置数据位失败: " + error.message());
+    serial.set_option(boost::asio::serial_port_base::flow_control(boost::asio::serial_port_base::flow_control::none),error);//流控(当前不使用流控)
+    if(error) throw std::runtime_error("设置流控失败: " + error.message());
+    serial.set_option(boost::asio::serial_port_base::parity(boost::asio::serial_port_base::parity::none),error);//校验位
+    if(error) throw std::runtime_error("设置校验位失败: " + error.message());
+    serial.set_option(boost::asio::serial_port_base::stop_bits(boost::asio::serial_port_base::stop_bits::one),error);//停止位,每一帧数据的结束符,当前使用一位停止位
+    if(error) throw std::runtime_error("设置停止位失败: " + error.message());
+
+    std::cout << port << " 串口打开且设置成功" << std::endl;
 }
 
+SerialPortComm::~SerialPortComm(){
+    std::cout << "释放资源" << std::endl;
+    if(serial.is_open()){
+        serial.close();
+    }
+}
+
+//异常处理函数
+int SerialPortComm::handle_error(const std::string &msg,const boost::system::error_code &error){
+    std::cout << msg << ' ' << error.message() << std::endl;
+    std::cout << "错误码: " << error.value() << std::endl;
+    if(serial.is_open()){
+        serial.close();
+    }
+    return -1;
+}
 
 }
 
--- src/mycar_driver/src/driver.cpp
+++ src/mycar_driver/src/driver.cpp
@@ -8,6 +8,10 @@
         1.创建头文件,实现串口通信的相关功能
         2.当前源文件,调用头文件,关注的是ros2相关业务逻辑
         这样设计的优点,提高内聚性,降低耦合性,方便后期的维护和拓展
+    阶段二: 编写并测试头文件功能
+        1.打开以及关闭串口
+        2.读实现
+        3.写实现
    
 */
 
@@ -20,7 +24,7 @@ public:
         RCLCPP_INFO(this->get_logger(),"namesapce:  node: %s 节点创建成功",str1.c_str());
 
         //实例化串口通信对象
-        serial_port_ = std::make_shared<my_serial::SerialPortComm>();
+        serial_port_ = std::make_shared<my_serial::SerialPortComm>("/dev/mycar",115200,8);
 
     }
 
